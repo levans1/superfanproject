748d9597e8dff1db7e12fbd740be5743f836c4bb
levans1 <45011820+levans1@users.noreply.github.com>
2019-11-08 14:05:02 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// JavaScript is capitalized using "camel case": https://en.wikipedia.org/wiki/Camel_case
+// Check out the power of the classList property: https://www.w3schools.com/jsref/prop_element_classlist.asp
+
+// "elem" is an abbreviation for "the element that was clicked in the HTML"
+// this is where the function is defined 
+ 
+
+// These W3Schools links show efficient ways of completing some of the challenges!
+// https://www.w3schools.com/jsref/prop_style_height.asp
+// https://www.w3schools.com/jsref/prop_html_innerhtml.asp
+// https://www.w3schools.com/js/tryit.asp?filename=tryjs_visibility
